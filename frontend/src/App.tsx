1fa37ceb002a14cab26f1c380a28dd3dec73853f
luke <blueskins@gmail.com>
2021-01-05 19:04:53 -0500

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
         <p> 
-=======
-        <p>
->>>>>>> parent of d8f5ae4... test
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
