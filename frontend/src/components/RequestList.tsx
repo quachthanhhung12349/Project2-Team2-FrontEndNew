1cd1013b94efa261e5397b665c6003d2ad2ffb3f
Hamza <hamza.0096@yahoo.com>
2021-01-10 22:26:08 -0700

    can get request data and patient data

--- frontend/src/components/RequestList.tsx
+++ frontend/src/components/RequestList.tsx
@@ -3,22 +3,33 @@ import axios from 'axios'
 import PatientNavBar from './PatientNavBar'
 
 
+interface IPatient{
+    healthCardNumber:number,
+    firstname:string
+}
+
 interface IReqList{
-    problem:string
+    patientId:IPatient,
+    problem:string,
+    responsed:boolean,
+    timeStamp:string
 }
 
 export const RequestList:React.FunctionComponent<any> = () => {
 
     const [data, setRequestList] = useState<IReqList[]>([])
+    const [pinfo, setPatientInfo] = useState<IPatient[]>([])
+    
 
     useEffect(() => {
         axios.get<IReqList[]>("http://localhost:8080/patient/1").then(
             res =>{
                 setRequestList(res.data)
                 console.log(res.data);
-                
+                setPatientInfo(data.map(pi => pi.patientId))
             }
         )
+        
     }, [])
 
     
@@ -27,9 +38,15 @@ export const RequestList:React.FunctionComponent<any> = () => {
                 <div>Get Request list
                 <p>
                 <ul>
-                    { data.map(text => 
-                     <li>{text.problem}</li>
+                    {data.map(text => 
+                     <li>{text.problem}, {String(text.responsed)}, 
+                        </li>
                     )}
+                    {pinfo.map(pi =>
+                       <li>{String(pi.firstname )} 
+                       </li> 
+                    )}
+                    
                 </ul>
                 </p>
                 </div>
