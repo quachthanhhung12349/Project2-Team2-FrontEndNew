ea8dbc779aec6f3ff62f599c917d32d1e8b1929a
haochengca <blueskins@gmail.com>
2021-01-13 19:41:51 -0500

    forum finished

--- frontend/src/components/forum/Forum.tsx
+++ frontend/src/components/forum/Forum.tsx
@@ -16,17 +16,36 @@ import TextField from "@material-ui/core/TextField";
 import { postTopic, postMessage } from "../../remote/remote-functions";
 import moment from "moment";
 import { useLocation } from "react-router-dom";
+// RCE CSS
+import "react-chat-elements/dist/main.css";
+// MessageBox component
+import {
+  MessageBox,
+  MessageList,
+  ChatList,
+  ChatItem,
+  Input,
+} from "react-chat-elements";
+import '../../assets/primary.scss';
+
+
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      width: "60%",
-      maxWidth: "36ch",
+      width: "100%",
       backgroundColor: theme.palette.background.paper,
+      margin:"auto",
     },
     inline: {
       display: "inline",
     },
+    bubble:{
+      width: "-webkit-fill-available",
+      backgroundColor:"red"
+    },
+    
+    
   })
 );
 
@@ -77,10 +96,11 @@ export const Forum: React.FC = (props) => {
     const topicstring = {
       topic: inputTopic,
       username: user.username,
+      role: user.role,
     };
     const data = await postTopic(topicstring);
     console.log(data);
-    
+
     setForums([...forums, data]);
     setInputTopic("");
     setInputShow(false);
@@ -98,27 +118,29 @@ export const Forum: React.FC = (props) => {
       user.role === "Patient"
         ? {
             message: inputMessage,
+            fromusername:user.username,
             forumId: {
               forumId: currentTopic,
             },
-            patientId:{
-              patientId:user.patientId,
-              username:user.username
-            }
+            patientId: {
+              patientId: user.patientId,
+              username: user.username,
+            },
           }
         : {
             message: inputMessage,
+            fromusername:user.username,
             forumId: {
               forumId: currentTopic,
             },
-            doctorId:{
-              doctorId:user.doctorId,
-              username:user.username
-            }
+            doctorId: {
+              doctorId: user.doctorId,
+              username: user.username,
+            },
           };
     const data = await postMessage(currentInputMessage);
     console.log(data);
-    
+
     setInputMessage("");
     setMessage([...message, currentInputMessage]);
   };
@@ -126,9 +148,12 @@ export const Forum: React.FC = (props) => {
   const classes = useStyles();
 
   function getContent() {
+    
     location.state.patientInfo !== undefined
       ? setUser(location.state.patientInfo)
       : setUser(location.state.doctorInfo);
+      console.log(user);
+      
   }
 
   useEffect(() => {
@@ -143,6 +168,8 @@ export const Forum: React.FC = (props) => {
 
         setForums(data);
         console.log(data);
+        
+        
       } catch (e) {
         console.log(e.message);
       }
@@ -151,7 +178,7 @@ export const Forum: React.FC = (props) => {
   }, []);
 
   return (
-    <div>
+    <div style={{margin:"auto", width:"960px",borderStyle:"solid",borderColor:"lightblue"}}>
       {isShow ? (
         <Button
           variant="contained"
@@ -163,56 +190,28 @@ export const Forum: React.FC = (props) => {
           Back
         </Button>
       ) : null}
-      {isShow ? <Typography variant="h4">{topic}</Typography> : null}
-      <Grid container spacing={3}>
+      {isShow ? <div><h1 style={{textAlign:"center"}}>{topic}</h1><Divider variant="inset" component="li" /></div> : null}
+      <Grid container>
         {!isShow ? (
-          <Grid item xs={4}>
+          <Grid item xs={12}>
             <Button
+              variant="outlined"
               onClick={() => {
                 setInputShow(true);
               }}
             >
               New Post
             </Button>
-            <List className={classes.root}>
-              {forums.map((r, index) => (
-                <ListItem
-                  alignItems="flex-start"
-                  onClick={() => getMessagesByForumId(r.forumId, r.topic)}
-                >
-                  <ListItemText
-                    primary={<Typography variant="h4">{r.topic}</Typography>}
-                    secondary={
-                      <React.Fragment>
-                        <Typography
-                          component="span"
-                          variant="body2"
-                          className={classes.inline}
-                          color="textPrimary"
-                        >
-                          by:{r.username}
-                          <br />
-                        </Typography>
-                        Submitted:
-                        {moment(r.timeStamp).format("YYYY-MM-DD HH:mm:ss")}
-                      </React.Fragment>
-                    }
-                  />
-                </ListItem>
-              ))}
-
-              <Divider variant="inset" component="li" />
-            </List>
-          </Grid>
-        ) : null}
-        {inputShow ? (
-          <div>
+            {inputShow ? (
+          <div style={{textAlign:"center",marginTop:"30px"}}>
             <TextField
               id="standard-basic"
               label="Input your topic"
-              name="topic"
+              fullWidth
               value={inputTopic}
               onChange={handleTopicChange}
+              multiline
+              variant="outlined"
             />
             <br />
             <Button
@@ -221,46 +220,103 @@ export const Forum: React.FC = (props) => {
               type="submit"
               onClick={() => onPostTopic()}
             >
-              Submit
+              Post
             </Button>
             <Button
               variant="contained"
-              color="primary"
+              color="secondary"
               onClick={() => setInputShow(false)}
             >
               Cancel
             </Button>
+            
           </div>
         ) : null}
+            <List className={classes.root}>
+              {forums.map((r, index) => (
+                // <ListItem
+                //   alignItems="flex-start"
+                //   onClick={() => getMessagesByForumId(r.forumId, r.topic)}
+                // >
+                //   <ListItemText
+                //     primary={<Typography variant="h4">{r.topic}</Typography>}
+                //     secondary={
+                //       <React.Fragment>
+                //         <Typography
+                //           component="span"
+                //           variant="body2"
+                //           className={classes.inline}
+                //           color="textPrimary"
+                //         >
+                //           by:{r.username}
+                //           <br />
+                //         </Typography>
+                //         Submitted:
+                //         {moment(r.timeStamp).format("YYYY-MM-DD HH:mm:ss")}
+                //       </React.Fragment>
+                //     }
+                //   />
+                // </ListItem>
+                <ChatItem
+                  avatar={r.role!=="Patient"?"https://www.cliparthut.com/images/150/FPesi.png":"https://www.cliparthut.com/images/149/WDCFg.png"}
+                  alt={r.username}
+                  title={r.topic}
+                  subtitle={"by:"+r.username}
+                  dateString={moment(r.timeStamp).format("YYYY-MM-DD HH:mm:ss")}
+                  onClick={() => getMessagesByForumId(r.forumId, r.topic)}
+                />
+              ))}
+
+              <Divider variant="inset" component="li" />
+            </List>
+          </Grid>
+        ) : null}
+        
         {/* <Button onClick= {()=>getMessagesByForumId(3)}>click</Button> */}
         {loading ? <Typography variant="h4">Loading......</Typography> : null}
         {isShow ? (
-          <Grid item xs={8}>
+          <Grid item xs={12}>
             {!loading
-              ? message.map((m, index) => (
-                  <Message
-                    message={m.message}
-                    timestamp={moment(m.timeStamp).format(
-                      "YYYY-MM-DD HH:mm:ss"
-                    )}
-                    doctor={!m.doctorId ? "" : m.doctorId.username}
-                    patient={!m.patientId ? "" : m.patientId.username}
+              ? 
+              // message.map((m, index) => (
+              //       <Message
+              //         message={m.message}
+              //         timestamp={moment(m.timeStamp).format(
+              //           "YYYY-MM-DD HH:mm:ss"
+              //         )}
+              //         doctor={!m.doctorId ? "" : m.doctorId.username}
+              //         patient={!m.patientId ? "" : m.patientId.username}
+              //       />
+              //     ))
+                message.map((m, index) => (
+                  <MessageBox
+                    
+                    type={"text"}
+                    text={"by:"+m.fromusername}
+                    title={m.message}
+                    dateString={moment(m.timeStamp).format("YYYY-MM-DD HH:mm:ss")}
+                    position={user.username!=m.fromusername?"left":"right"}
                   />
                 ))
               : null}
             {!loading ? (
-              <div>
+              <div style={{float:"right",marginTop:"30px",textAlign:"center"}}>
                 <TextField
                   id="standard-basic"
                   label="Write your post"
                   name="message"
+                  
                   value={inputMessage}
                   onChange={handleMessageChange}
+                  variant="outlined"
+                  
                 />
                 <Button
                   variant="contained"
                   color="primary"
                   onClick={onPostMessage}
+                  style={{float:"right",marginTop:"15px"}}
+                  
                 >
                   Submit
                 </Button>
